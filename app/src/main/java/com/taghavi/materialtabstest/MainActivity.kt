7268129e2f7193c15a60e8e8e81129c7f2c124f4
EhsanTgv <EhsanTgv@gmail.com>
2021-01-05 13:21:52 +0330

    Implement ViewBinding

--- app/src/main/java/com/taghavi/materialtabstest/MainActivity.kt
+++ app/src/main/java/com/taghavi/materialtabstest/MainActivity.kt
@@ -2,10 +2,29 @@ package com.taghavi.materialtabstest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import com.taghavi.materialtabstest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // Handle tab select
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Handle tab reselect
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Handle tab unselect
+            }
+        })
     }
 }
